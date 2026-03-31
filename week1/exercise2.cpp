ef1cb7dadff0a2387e0e3f62847575afadd2ac24
trantoan1103 <trantoan110305@gmail.com>
2026-03-31 23:27:38 +0700

    some fixign for exercise 2

--- week1/exercise2.cpp
+++ week1/exercise2.cpp
@@ -7,7 +7,7 @@ public:
         stack<char> st; 
 
         for(char c : s){ 
-            if(c == '(' || c == '[' || c == '[' ){
+            if(c == '(' || c == '[' || c == '{' ){
                 st.push(c);
             } else{ 
                 if(st.empty())return false;
